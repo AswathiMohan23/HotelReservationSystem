fd44fe980382046ca0c8dd10dbb9e166cf660026
AswathiMohan23 <achu999here@gmail.com>
2022-12-05 19:24:39 +0530

    [ADDED] : code to find the cheapest hotel

--- src/main/java/com/java/HotelReservationSystem.java
+++ src/main/java/com/java/HotelReservationSystem.java
@@ -1,5 +1,7 @@
 package com.java;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class HotelReservationSystem {
@@ -8,6 +10,13 @@ public class HotelReservationSystem {
     public static List reservationLakeWood=new ArrayList();
     public static List reservationBridgeWood=new ArrayList();
     public static List reservationRidgeWood=new ArrayList();
+    public static long noOfDays=0;
+
+    public static HotelDetails lakeWood=new HotelDetails("lakeWood",110.00,80.00,90.00,80.00,3);
+    public static HotelDetails bridgewood=new HotelDetails("bridgewood",160,110,90,80,4);
+    public static HotelDetails ridgewood=new HotelDetails("ridgewood",220,100,150,40,5);
+
+
 
     List hotelDetails=new ArrayList();
 
@@ -15,19 +24,50 @@ public class HotelReservationSystem {
 
     Map <Integer,List> hotelMap=new HashMap<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         System.out.println("\n\t\t\t\t Welcome to hotel reservation\n\t\t\t\t******************************\n");
         creatingList();
+        findingCheapestHotel();
 
 
     }
 
+    private static void findingCheapestHotel() throws ParseException {
+        Date daysOfStay = calculatingTheDaysOfStay();
+        if ((daysOfStay.toString().contains("Mon")) || (daysOfStay.toString().contains("Tues")) || (daysOfStay.toString().contains("Wed"))
+                || (daysOfStay.toString().contains("thurs")) || (daysOfStay.toString().contains("Fri"))) {
+            double lakeWoodNormal = lakeWood.getWeekDayRateNormal();
+            double bridgeWoodNormal = bridgewood.getWeekDayRateNormal();
+            double ridgeWoodNormal = ridgewood.getWeekDayRateNormal();
+            if ((lakeWoodNormal < bridgeWoodNormal) && (lakeWoodNormal < ridgeWoodNormal))
+                System.out.println("\n======================================== Cheapest is LakeWood =  " + lakeWood.getWeekDayRateNormal() + " $ ========================================");
+            else if ((bridgeWoodNormal < lakeWoodNormal) && (bridgeWoodNormal < ridgeWoodNormal))
+                System.out.println("\n======================================== Cheapest is BridgeWood =  " + bridgewood.getWeekDayRateNormal() + " $ ========================================");
+            else
+                System.out.println("\n======================================== Cheapest is RidgeWood =  " + ridgewood.getWeekDayRateNormal() + " $ ========================================");
+        }
+    }
+
+    private static Date calculatingTheDaysOfStay() throws ParseException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(
+            "dd-MM-yyyy");
+
+        String checkIn = "10-09-2020 01:10:20";
+        String checkOut = "11-09-2020 06:30:50";
+        Date d1 = dateFormat.parse(checkIn);
+        Date d2 = dateFormat.parse(checkOut);
+        long differenceInTime = d2.getTime() - d1.getTime();
+        noOfDays = (differenceInTime / (1000 * 60 * 60 * 24)) % 365;
+        Date dayOfStay = (new Date(2020, Calendar.SEPTEMBER, 10));
+        System.out.println("no of days of stay = "+noOfDays+" day");
+        System.out.println("The day is : "+dayOfStay);
+        return dayOfStay;
+
+    }
+
     public static void creatingList() {
-        HotelDetails lakeWood=new HotelDetails("lakeWood",110.00,80.00,90.00,80.00,3);
         addingDetails(lakeWood,reservationLakeWood);
-        HotelDetails bridgewood=new HotelDetails("bridgewood",160,110,90,80,4);
         addingDetails(bridgewood,reservationBridgeWood);
-        HotelDetails ridgewood=new HotelDetails("ridgewood",220,100,150,40,5);
         addingDetails(ridgewood,reservationRidgeWood);
         reservationSystemMap.put(1, reservationLakeWood);
         reservationSystemMap.put(2, reservationBridgeWood);
